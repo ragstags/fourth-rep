ec9205cf63e0320fdb0973719b5f1e846546cb3b
ragstags <shazia.alphaauth1@gmail.com>
2018-12-22 17:18:37 +0000

    testing new piece

--- src/com/practice/Simple.java
+++ src/com/practice/Simple.java
@@ -7,5 +7,7 @@ public class Simple {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("some more practice");
+        int a =50;
     }
 }
