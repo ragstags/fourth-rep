f15244219c8dac94e38baa14c7c2e6e2dacd545e
ragstags <shazia.alphaauth1@gmail.com>
2018-12-22 10:45:09 +0000

    trying again

---
+++ src/com/practice/Simple.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+/**
+ * Created by nadimchouglay on 22/12/2018.
+ */
+public class Simple {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
