06f3904dc01ac41e863556766e73d21bc9df3eab
shanad01 <shazi018@gmail.com>
2018-12-25 11:35:06 +0000

    adding hello file

---
+++ src/com/practice/hello.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+/**
+ * Created by nadimchouglay on 25/12/2018.
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
